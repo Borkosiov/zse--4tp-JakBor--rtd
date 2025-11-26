bc21ec76043304056e29d35686c1b49a884ec653
Jakub B 4tp <133799187+Borkosiov@users.noreply.github.com>
2025-11-26 19:47:52 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,9 @@
-# Configuration file for the Sphinx documentation builder.
+project = 'dokumentacja RtD'
+author = 'Jakub Borkowski 4tp'
+release = '1.0'
 
-# -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
 
-release = '0.1'
-version = '0.1.0'
-
-# -- General configuration
-
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
-
-# -- Options for HTML output
-
+extensions = []
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+master_doc = 'index'
